42f116b891d40eb0157eac3008063d4a1d98cd47
René Peña <rpenav91@hotmail.com>
2014-03-26 08:12:25 -0600

    Deleted files
    Some files where deleted

--- menu3/js/product.js
+++
@@ -1,8 +0,0 @@
-﻿//----------------------------------------------------------------
-// product class
-function product(sku, name, description, price) {
-    this.sku = sku; // product code (SKU = stock keeping unit)
-    this.name = name;
-    this.description = description;
-    this.price = price;
-}
